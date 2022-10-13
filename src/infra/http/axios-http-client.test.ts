b0ab8a10d6602677ccf39bfee8b9d0e7b3756743
Welly Mayara <welly.maya.s@gmail.com>
2022-10-13 15:58:40 -0300

    refactor: move SUT creation to an method

--- src/infra/http/axios-http-client.test.ts
+++ src/infra/http/axios-http-client.test.ts
@@ -1,14 +1,20 @@
 import { AxiosHttpClient } from './axios-http-client';
 import axios from 'axios';
+import { faker } from '@faker-js/faker';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient();
+};
+
 describe('AxiosHttpClient', () => {
   it('Should call axios with the correct URL', async () => {
-    const sut = new AxiosHttpClient();
-    await sut.post({ url: 'any_url' });
+    const url = faker.internet.url();
+    const sut = makeSut();
+    await sut.post({ url: url });
 
-    expect(mockedAxios).toHaveBeenCalledWith('any_url');
+    expect(mockedAxios).toHaveBeenCalledWith(url);
   });
 });
