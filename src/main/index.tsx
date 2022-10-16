1d357d967198d576f51a9f13c6733656febf633b
Welly Mayara <welly.maya.s@gmail.com>
2022-10-16 17:53:06 -0300

    chore: add sass

---
+++ src/main/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Login from '@/presentation/pages/login/login';
+
+ReactDOM.render(<Login />, document.getElementById('main'));
---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styles from './login-styles.scss';
+
+const Login = () => {
+  return (
+    <div className={Styles.login}>
+      <h1>hello world!</h1>
+    </div>
+  );
+};
+
+export default Login;
